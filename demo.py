c28b4172784978af93aa3bf8e621de5b4083c5c5
NandaKishoreJana <110282188+NandaKishoreJana@users.noreply.github.com>
2023-12-28 16:14:04 +0530

    python code

---
+++ demo.py
@@ -0,0 +1,15 @@
+n = 10
+num1 = 0
+num2 = 1
+next_number = num2  
+count = 1
+ 
+while count <= n:
+    print(next_number, end=" ")
+    count += 1
+    num1, num2 = num2, next_number
+    next_number = num1 + num2
+print()
+
+#this is the code for the fibonacci series in the given status of operation format
+#python.py the file is done
